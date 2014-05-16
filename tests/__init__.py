7711da0941a88ea42223bf53880228cbad6004fe
Christian Haselgrove <christian.haselgrove@umassmed.edu>
2014-05-16 18:22:30 -0400

    restructured data tests

---
+++ tests/__init__.py
@@ -0,0 +1,94 @@
+# See file COPYING distributed with dpf for copyright and license.
+
+import os
+import subprocess
+import socket
+import time
+
+class ServerError(Exception):
+    """server error"""
+
+def start_data_server():
+
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+    if not os.path.exists('tmp/data'):
+        os.mkdir('tmp/data')
+
+    fo_out = open('tmp/data_test.stdout', 'a')
+    fo_err = open('tmp/data_test.stderr', 'a')
+
+    po = subprocess.Popen(['dpf_data_server', 
+                           '-C', 'tmp/data', 
+                           '-H', 'dpf.data.handlers.CSVHandler', 
+                           '-H', 'dpf.data.handlers.JSONHandler'],
+                          stdout=fo_out, 
+                          stderr=fo_err)
+
+    # wait for the data server to be listening
+    # if it doesn't come up after a certain amount of time, clean up and 
+    # raise an error
+
+    for i in xrange(5):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect(('localhost', 8080))
+        except socket.error:
+            time.sleep(1)
+            pass
+        else:
+            break
+        finally:
+            s.close()
+    else:
+        po.terminate()
+        fo_out.close()
+        fo_err.close()
+        raise ServerError()
+
+    return (po, fo_out, fo_err)
+
+def start_process_server():
+
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+    if not os.path.exists('tmp/process'):
+        os.mkdir('tmp/process')
+    fo_out = open('tmp/process_test.stdout', 'a')
+    fo_err = open('tmp/process_test.stderr', 'a')
+    po = subprocess.Popen(['dpf_process_server', 
+                           '-C', 'tmp/process', 
+                           '-H', '/wc=dpf.process.handlers.WCHandler'], 
+                          stdout=fo_out, 
+                          stderr=fo_err)
+
+    # wait for the data server to be listening
+    # if it doesn't come up after a certain amount of time, clean up and 
+    # raise an error
+
+    for i in xrange(5):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect(('localhost', 8081))
+        except socket.error:
+            time.sleep(1)
+            pass
+        else:
+            break
+        finally:
+            s.close()
+    else:
+        po.terminate()
+        fo_out.close()
+        fo_err.close()
+        raise ServerError()
+
+    return (po, fo_out, fo_err)
+
+def stop_server(po, fo_out, fo_err):
+    po.terminate()
+    fo_out.close()
+    fo_err.close()
+    return
+
+# eof
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,107 +1,56 @@
 # See file COPYING distributed with dpf for copyright and license.
 
-import os
-import subprocess
-import socket
-import time
 import uuid
 import httplib
 import json
+from . import start_data_server, stop_server
 
-class test_data:
+test_vars = {}
 
-    @classmethod
-    def setUpClass(cls):
+def setup():
+    po, fo_out, fo_err = start_data_server()
+    test_vars['po'] = po
+    test_vars['fo_out'] = fo_out
+    test_vars['fo_err'] = fo_err
+    return
 
-        """set up the data test class
+def teardown():
+    stop_server(test_vars['po'], test_vars['fo_out'], test_vars['fo_err'])
+    return
 
-        this will create tmp/data if needed and start the data server
-        """
+class BaseDataTest:
 
-        if not os.path.exists('tmp'):
-            os.mkdir('tmp')
-        if not os.path.exists('tmp/data'):
-            os.mkdir('tmp/data')
-        cls.fo_out = open('tmp/data_test.stdout', 'a')
-        cls.fo_err = open('tmp/data_test.stderr', 'a')
-        cls.po = subprocess.Popen(['dpf_data_server', 
-                                   '-C', 'tmp/data', 
-                                   '-H', 'dpf.data.handlers.CSVHandler', 
-                                   '-H', 'dpf.data.handlers.JSONHandler'],
-                                  stdout=cls.fo_out, 
-                                  stderr=cls.fo_err)
-
-        # wait for the data server to be listening
-        # if it doesn't come up after a certain amount of time, clean up and 
-        # then assert False to indicate an error
-
-        for i in xrange(5):
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                s.connect(('localhost', 8080))
-            except socket.error:
-                time.sleep(1)
-                pass
-            else:
-                break
-            finally:
-                s.close()
-        else:
-            cls.po.terminate()
-            cls.fo_out.close()
-            cls.fo_err.close()
-            assert False
-        return
-
-    @classmethod
-    def tearDownClass(cls):
-
-        """clean up after the data tests
-
-        this just stops the data server and closes its output file objects
-
-        the server cache directory is left in place
-        """
-        
-        cls.po.terminate()
-        cls.fo_out.close()
-        cls.fo_err.close()
-        return
-
+    n_connections = 0
 
     def setUp(self):
-
-        """set up each test
-
-        prepare the connections to the data server
-        """
-
-        self.hc = httplib.HTTPConnection('localhost', '8080')
-        self.hc2 = httplib.HTTPConnection('localhost', '8080')
-        self.hc3 = httplib.HTTPConnection('localhost', '8080')
-        self.hc4 = httplib.HTTPConnection('localhost', '8080')
-        self.hc5 = httplib.HTTPConnection('localhost', '8080')
-        self.hc6 = httplib.HTTPConnection('localhost', '8080')
+        """set up each test"""
+        self.connections = []
+        self.next_connection = 0
+        for i in xrange(self.n_connections):
+            self.connections.append(httplib.HTTPConnection('localhost', 8080))
         return
 
     def tearDown(self):
-
         """clean up after each test"""
-
-        self.hc.close()
-        self.hc2.close()
-        self.hc3.close()
-        self.hc4.close()
-        self.hc5.close()
-        self.hc6.close()
+        for i in xrange(self.n_connections):
+            self.connections[i].close()
         return
 
-    def test_get_root(self):
+    def request(self, *args):
+        hc = self.connections[self.next_connection]
+        self.next_connection += 1
+        hc.request(*args)
+        return hc.getresponse()
+
+class TestGetRoot(BaseDataTest):
+
+    n_connections = 1
+
+    def test(self):
 
         """test GET / (should return 405 Method Not Allowed)"""
 
-        self.hc.request('GET', '/')
-        r = self.hc.getresponse()
+        r = self.request('GET', '/')
         headers = dict(r.getheaders())
         assert r.status == 405
         assert r.reason == 'Method Not Allowed'
@@ -110,12 +59,15 @@ class test_data:
         assert r.read() == ''
         return
 
-    def test_404(self):
+class Test404(BaseDataTest):
 
-        """test GET /somethingbogus (should return 404 Not Found)"""
+    """test GET /somethingbogus (should return 404 Not Found)"""
 
-        self.hc.request('GET', '/thisshouldnotexist')
-        r = self.hc.getresponse()
+    n_connections = 1
+
+    def test(self):
+
+        r = self.request('GET', '/thisshouldnotexist')
         headers = dict(r.getheaders())
         assert r.status == 404
         assert r.reason == 'Not Found'
@@ -124,14 +76,17 @@ class test_data:
         assert r.read() == ''
         return
 
-    def test_post_get_delete(self):
+class TestPostGetDelete(BaseDataTest):
 
-        """test POST/GET/DELETE"""
+    """test POST/GET/DELETE"""
+
+    n_connections = 4
+
+    def test(self):
 
         data = str(uuid.uuid4())
 
-        self.hc.request('POST', '/', data)
-        r = self.hc.getresponse()
+        r = self.request('POST', '/', data)
         assert r.status == 201
         assert r.reason == 'Created'
         headers = dict(r.getheaders())
@@ -141,8 +96,7 @@ class test_data:
         ident = headers['location'].split('/')[-1]
         assert r.read() == ''
 
-        self.hc2.request('GET', '/%s' % ident)
-        r = self.hc2.getresponse()
+        r = self.request('GET', '/%s' % ident)
         assert r.status == 200
         assert r.reason == 'OK'
         headers = dict(r.getheaders())
@@ -153,14 +107,12 @@ class test_data:
             self.fail('content-length is not an integer')
         assert r.read() == data
 
-        self.hc2.request('DELETE', '/%s' % ident)
-        r = self.hc2.getresponse()
+        r = self.request('DELETE', '/%s' % ident)
         assert r.status == 204
         assert r.reason == 'No Content'
         assert r.read() == ''
 
-        self.hc2.request('GET', '/%s' % ident)
-        r = self.hc2.getresponse()
+        r = self.request('GET', '/%s' % ident)
         assert r.status == 410
         assert r.reason == 'Gone'
         headers = dict(r.getheaders())
@@ -174,19 +126,21 @@ class test_data:
 
         return
 
-    def test_media_type(self):
+class TestMediaType(BaseDataTest):
 
-        """test media types"""
+    """test media types"""
+
+    n_connections = 6
+
+    def test(self):
 
         data = str(uuid.uuid4())
 
-        self.hc.request('POST', '/', data, {'Content-Type': 'ctmaj/ctmin'})
-        r = self.hc.getresponse()
+        r = self.request('POST', '/', data, {'Content-Type': 'ctmaj/ctmin'})
         headers = dict(r.getheaders())
         ident = headers['location'].split('/')[-1]
 
-        self.hc2.request('GET', '/%s' % ident, '', {'Accept': '*/*'})
-        r = self.hc2.getresponse()
+        r = self.request('GET', '/%s' % ident, '', {'Accept': '*/*'})
         assert r.status == 200
         assert r.reason == 'OK'
         headers = dict(r.getheaders())
@@ -194,8 +148,7 @@ class test_data:
         assert headers['content-type'] == 'ctmaj/ctmin'
         assert r.read() == data
 
-        self.hc3.request('GET', '/%s' % ident, '', {'Accept': 'ctmaj/*'})
-        r = self.hc3.getresponse()
+        r = self.request('GET', '/%s' % ident, '', {'Accept': 'ctmaj/*'})
         assert r.status == 200
         assert r.reason == 'OK'
         headers = dict(r.getheaders())
@@ -203,8 +156,7 @@ class test_data:
         assert headers['content-type'] == 'ctmaj/ctmin'
         assert r.read() == data
 
-        self.hc4.request('GET', '/%s' % ident, '', {'Accept': 'ctmaj/ctmin'})
-        r = self.hc4.getresponse()
+        r = self.request('GET', '/%s' % ident, '', {'Accept': 'ctmaj/ctmin'})
         assert r.status == 200
         assert r.reason == 'OK'
         headers = dict(r.getheaders())
@@ -212,41 +164,44 @@ class test_data:
         assert headers['content-type'] == 'ctmaj/ctmin'
         assert r.read() == data
 
-        self.hc5.request('GET', '/%s' % ident, '', {'Accept': 'text/*'})
-        r = self.hc5.getresponse()
+        r = self.request('GET', '/%s' % ident, '', {'Accept': 'text/*'})
         assert r.status == 406
         assert r.reason == 'Not Acceptable'
         assert r.read() == ''
 
-        self.hc6.request('GET', '/%s' % ident, '', {'Accept': 'text/plain'})
-        r = self.hc6.getresponse()
+        r = self.request('GET', '/%s' % ident, '', {'Accept': 'text/plain'})
         assert r.status == 406
         assert r.reason == 'Not Acceptable'
         assert r.read() == ''
 
-    def test_validator(self):
+class TestValidator(BaseDataTest):
 
-        """test a media type validator"""
+    """test a media type validator"""
 
-        self.hc.request('POST', '/', ',', {'Content-Type': 'text/json'})
-        r = self.hc.getresponse()
+    n_connections = 1
+
+    def test(self):
+
+        r = self.request('POST', '/', ',', {'Content-Type': 'text/json'})
         assert r.status == 400
         assert r.reason == 'Bad Request'
 
-    def test_converter(self):
+class TestConverter(BaseDataTest):
 
-        """test a media type converter"""
+    """test a media type converter"""
+
+    n_connections = 2
+
+    def test(self):
 
         data = '1,2\na,b\n'
         json_data = '[["1", "2"], ["a", "b"]]'
 
-        self.hc.request('POST', '/', data, {'Content-Type': 'text/csv'})
-        r = self.hc.getresponse()
+        r = self.request('POST', '/', data, {'Content-Type': 'text/csv'})
         headers = dict(r.getheaders())
         ident = headers['location'].split('/')[-1]
 
-        self.hc2.request('GET', '/%s' % ident, '', {'Accept': 'text/json'})
-        r = self.hc2.getresponse()
+        r = self.request('GET', '/%s' % ident, '', {'Accept': 'text/json'})
         assert r.status == 200
         assert r.reason == 'OK'
         headers = dict(r.getheaders())
